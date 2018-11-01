3d47a460b22898fb2dfc7aa18b257d582b046a01
Kangjie <840416501@qq.com>
2018-11-01 14:29:03 +0800

    修改空置bug

--- app/src/main/java/com/dingwei/dingwei/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/dingwei/dingwei/ui/activity/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.dingwei.dingwei.ui.activity
 
 import com.dingwei.dingwei.R
 import com.dingwei.dingwei.base.BaseActivity
+import com.dingwei.dingwei.mvp.contract.LoginContract
+import com.dingwei.dingwei.mvp.model.bean.LoginBean
 import com.dingwei.dingwei.mvp.presenter.LoginPresenter
 import kotlinx.android.synthetic.main.activity_login_layout.*
 
@@ -9,11 +11,17 @@ import kotlinx.android.synthetic.main.activity_login_layout.*
 /**
  * Created by kangjie on 2018/10/24.
  */
-class LoginActivity : BaseActivity() {
+class LoginActivity : BaseActivity(),LoginContract.View {
+
+
     private val mPresenter by lazy { LoginPresenter() }
     override fun layoutId(): Int = R.layout.activity_login_layout
     override fun initView() {
-        login_btn.setOnClickListener {  }
+        login_btn.setOnClickListener {
+            var phone = login_name_et.text.toString().trim()
+            var password = login_password_et.text.toString().trim()
+            mPresenter.login(phone,password)
+        }
     }
 
     override fun start() {
@@ -21,6 +29,23 @@ class LoginActivity : BaseActivity() {
 
     override fun initData() {
     }
+
+    override fun showLoading() {
+
+    }
+
+    override fun dismissLoading() {
+
+    }
+
+    override fun showError(errorMsg: String) {
+
+    }
+
+    override fun setLoginView(loginBean: LoginBean) {
+
+    }
+
 
 
 
